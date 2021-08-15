90e9059f148767efbae0f4fdad6b5446a661d383
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-15 10:18:51 -0700

    Make multiple runs not error.

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
 set -e
-find ./ -name "*.dll" | xargs zstd --compress
+find ./ -name "*.dll" | xargs zstd --compress -q
 find ./ -name "*.dll" | xargs rm 
--- decompress.sh
+++ decompress.sh
@@ -1,5 +1,5 @@
 set -e
-find ./ -name "*.zst" | xargs zstd --uncompress
+find ./ -name "*.zst" | xargs zstd --uncompress -q
 find ./ -name "*.zst" | xargs rm 
 # Alternative for windows
-# zstd -d -r --rm ./sdk || true
+# zstd -d -r --rm . -q
