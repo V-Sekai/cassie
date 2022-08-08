5eba3102c7793f6a5847d69ebff3aa5e9f610645
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-08 07:38:30 -0700

    Remove dead code.

--- Assets/Scripts/Data/Constraints/SurfaceConstraint.cs
+++ Assets/Scripts/Data/Constraints/SurfaceConstraint.cs
@@ -5,23 +5,19 @@ public class SurfaceConstraint
 {
     public int PatchID { get; }
     public Vector3 StartPosition { get; } // in canvas space
-    //public float StartTime { get; }
     public bool LeftMidStroke { get; private set; } = false;
     public Vector3 EndPosition { get; private set; } // in canvas space
-    //public float EndTime { get; private set; }
 
     public SurfaceConstraint(int patchID, Vector3 startPosition)
     {
         this.PatchID = patchID;
         this.StartPosition = startPosition;
-        //this.StartTime = Time.time;
     }
 
     public void Leave(Vector3 position)
     {
         this.LeftMidStroke = true;
         this.EndPosition = position;
-        //this.EndTime = Time.time;
     }
 
 }
